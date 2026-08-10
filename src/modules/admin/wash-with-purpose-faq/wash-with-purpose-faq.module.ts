ccd5d17a86736e80a212ceef7355484a512bae36
Foysal Hasan <foysal3909@gmail.com>
2026-08-10 14:41:17 +0600

    feat: add ActivityLogModule to WashWithPurposeFaqModule imports

--- src/modules/admin/wash-with-purpose-faq/wash-with-purpose-faq.module.ts
+++ src/modules/admin/wash-with-purpose-faq/wash-with-purpose-faq.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { WashWithPurposeFaqService } from './wash-with-purpose-faq.service';
 import { WashWithPurposeFaqController } from './wash-with-purpose-faq.controller';
+import { ActivityLogModule } from 'src/activity-log/activity-log.module';
 
 @Module({
+  imports: [
+    ActivityLogModule
+  ],
   controllers: [WashWithPurposeFaqController],
   providers: [WashWithPurposeFaqService],
 })
-export class WashWithPurposeFaqModule {}
+export class WashWithPurposeFaqModule { }
